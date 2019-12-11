1887e0047075af5f34ce6fded7304775cf12172b
user1 <user1@DESKTOP-0IFG55Q>
2019-12-11 21:20:27 +0900

    서블릿 이름만 만듦-김예은

---
+++ kh2_semiProject/src/semi/selvlet/grandmaster/CouponDeleteServlet.java
@@ -0,0 +1,9 @@
+package semi.selvlet.grandmaster;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/grandmaster/coupondelete.do")
+public class CouponDeleteServlet extends HttpServlet{
+
+}
