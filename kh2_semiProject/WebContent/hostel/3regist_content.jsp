35279099b4d0070b97e6740ea45ab131c9305d61
leegayeong <blue2v@naver.com>
2019-12-18 18:00:24 +0900

    숙소 등록

---
+++ kh2_semiProject/WebContent/hostel/3regist_content.jsp
@@ -0,0 +1,70 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>숙소 설명을 등록해봅시다.</title>
+    
+    <style>
+        * {
+            box-sizing: border-box;
+        } 
+        .a {
+            width: 70%;
+            /* 내용물 가운데 정렬 */
+            text-align: center;
+            /* div 자신을 가운데 정렬 */
+            margin-left: auto;
+            margin-right: auto;
+        }
+      	.a>input[type=text] {
+            width: 80%;
+            height: 30rem;
+        }
+        .btn {
+            border: 0;
+            font-size: 18px;
+            display: block;
+            margin: 35px auto;
+            padding: 15px 20px;
+            background-color: lightsalmon;
+        }
+        .btn:hover {
+            background-color: white;
+            color: lightsalmon;
+            cursor: pointer;
+        }
+
+    </style>
+
+</head>
+
+<body>
+
+    <div class="a">
+
+        <h1 style="color: lightsalmon">숙소를 설명해주세요!</h1>
+
+        <table border="0" width="100%">
+
+            <tr>
+                <td colspan="2">
+                    <textarea name="content" required rows="30" cols="100" style="resize:vertical;"></textarea>
+                </td>
+            </tr>
+
+            <tr>
+                <th>첨부파일</th>
+                <td>
+                    <input type="file" name="file" accept=".jpg, .png, .gif">
+                </td>
+            </tr>
+
+        </table>
+
+        <a href="4regist_date.jsp"><input class="btn" type="button" value="다음"></a>
+
+    </div>
+
+</body></html>
---
+++ kh2_semiProject/src/beans/RoomInfoDto.java
@@ -0,0 +1,317 @@
+package beans;
+
+public class RoomInfoDto {
+
+	private int room_no;
+	private int hostel_no;
+	private int owner_no;
+	private int region_no;
+	private int room_price;
+	private int room_standard_people;
+	private int room_max_people;
+	private int room_bed;
+	private int room_bath;
+	private String room_spa;
+	private String room_cook;
+	private String room_content;
+	private String room_parking;
+	private String room_pet;
+	private String room_name;
+	private String room_basic;
+	private String room_tv;
+	private String room_cool;
+	private String room_hot;
+	private String room_dry;
+	private String room_breakfast;
+
+	public RoomInfoDto() {
+
+		super();
+
+	}
+
+	public RoomInfoDto(int room_no, int hostel_no, int owner_no, int region_no, int room_price,
+
+			int room_standard_people, int room_max_people, int room_bed, int room_bath, String room_spa,
+			String room_cook, String room_content, String room_parking, String room_pet, String room_name,
+			String room_basic, String room_tv, String room_cool, String room_hot, String room_dry,
+			String room_breakfast) {
+
+		super();
+
+		this.room_no = room_no;
+		this.hostel_no = hostel_no;
+		this.owner_no = owner_no;
+		this.region_no = region_no;
+		this.room_price = room_price;
+		this.room_standard_people = room_standard_people;
+		this.room_max_people = room_max_people;
+		this.room_bed = room_bed;
+		this.room_bath = room_bath;
+		this.room_spa = room_spa;
+		this.room_cook = room_cook;
+		this.room_content = room_content;
+		this.room_parking = room_parking;
+		this.room_pet = room_pet;
+		this.room_name = room_name;
+		this.room_basic = room_basic;
+		this.room_tv = room_tv;
+		this.room_cool = room_cool;
+		this.room_hot = room_hot;
+		this.room_dry = room_dry;
+		this.room_breakfast = room_breakfast;
+
+	}
+
+	public int getRoom_no() {
+		return room_no;
+
+	}
+
+	public void setRoom_no(int room_no) {
+
+		this.room_no = room_no;
+
+	}
+
+	public int getHostel_no() {
+
+		return hostel_no;
+
+	}
+
+	public void setHostel_no(int hostel_no) {
+
+		this.hostel_no = hostel_no;
+
+	}
+
+	public int getOwner_no() {
+
+		return owner_no;
+
+	}
+
+	public void setOwner_no(int owner_no) {
+
+		this.owner_no = owner_no;
+
+	}
+
+	public int getRegion_no() {
+
+		return region_no;
+
+	}
+
+	public void setRegion_no(int region_no) {
+
+		this.region_no = region_no;
+
+	}
+
+	public int getRoom_price() {
+
+		return room_price;
+
+	}
+
+	public void setRoom_price(int room_price) {
+
+		this.room_price = room_price;
+
+	}
+
+	public int getRoom_standard_people() {
+
+		return room_standard_people;
+
+	}
+
+	public void setRoom_standard_people(int room_standard_people) {
+
+		this.room_standard_people = room_standard_people;
+
+	}
+
+	public int getRoom_max_people() {
+
+		return room_max_people;
+
+	}
+
+	public void setRoom_max_people(int room_max_people) {
+
+		this.room_max_people = room_max_people;
+
+	}
+
+	public int getRoom_bed() {
+
+		return room_bed;
+
+	}
+
+	public void setRoom_bed(int room_bed) {
+
+		this.room_bed = room_bed;
+
+	}
+
+	public int getRoom_bath() {
+
+		return room_bath;
+
+	}
+
+	public void setRoom_bath(int room_bath) {
+
+		this.room_bath = room_bath;
+
+	}
+
+	public String getRoom_spa() {
+
+		return room_spa;
+
+	}
+
+	public void setRoom_spa(String room_spa) {
+
+		this.room_spa = room_spa;
+
+	}
+
+	public String getRoom_cook() {
+
+		return room_cook;
+
+	}
+
+	public void setRoom_cook(String room_cook) {
+
+		this.room_cook = room_cook;
+
+	}
+
+	public String getRoom_content() {
+
+		return room_content;
+
+	}
+
+	public void setRoom_content(String room_content) {
+
+		this.room_content = room_content;
+
+	}
+
+	public String getRoom_parking() {
+
+		return room_parking;
+
+	}
+
+	public void setRoom_parking(String room_parking) {
+
+		this.room_parking = room_parking;
+
+	}
+
+	public String getRoom_pet() {
+
+		return room_pet;
+
+	}
+
+	public void setRoom_pet(String room_pet) {
+
+		this.room_pet = room_pet;
+
+	}
+
+	public String getRoom_name() {
+
+		return room_name;
+
+	}
+
+	public void setRoom_name(String room_name) {
+
+		this.room_name = room_name;
+
+	}
+
+	public String getRoom_basic() {
+
+		return room_basic;
+
+	}
+
+	public void setRoom_basic(String room_basic) {
+
+		this.room_basic = room_basic;
+
+	}
+
+	public String getRoom_tv() {
+
+		return room_tv;
+
+	}
+
+	public void setRoom_tv(String room_tv) {
+
+		this.room_tv = room_tv;
+
+	}
+
+	public String getRoom_cool() {
+
+		return room_cool;
+
+	}
+
+	public void setRoom_cool(String room_cool) {
+
+		this.room_cool = room_cool;
+
+	}
+
+	public String getRoom_hot() {
+
+		return room_hot;
+
+	}
+
+	public void setRoom_hot(String room_hot) {
+
+		this.room_hot = room_hot;
+
+	}
+
+	public String getRoom_dry() {
+
+		return room_dry;
+
+	}
+
+	public void setRoom_dry(String room_dry) {
+
+		this.room_dry = room_dry;
+
+	}
+
+	public String getRoom_breakfast() {
+
+		return room_breakfast;
+
+	}
+
+	public void setRoom_breakfast(String room_breakfast) {
+
+		this.room_breakfast = room_breakfast;
+
+	}
+
+}
