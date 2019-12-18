c04e4cedee3790027a39dd0afb52733d4c1f08c3
SungWoo824 <scody1219@gmail.com>
2019-12-18 19:42:27 +0900

    session listner 업로드

---
+++ kh2_semiProject/src/beans/VisitDao.java
@@ -0,0 +1,76 @@
+package beans;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class VisitDao {
+	private static DataSource source;
+	static {
+		try {
+			InitialContext ctx = new InitialContext();
+			source = (DataSource) ctx.lookup("java:comp/env/jdbc/oracle");
+
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public Connection getConnection() throws Exception {
+		return source.getConnection();
+	}
+
+	private static VisitDao instance;
+	    
+	  
+	    private VisitDao(){}
+	    public static VisitDao getInstance(){
+	        if(instance==null)
+	            instance=new VisitDao();
+	        return instance;
+	    }
+
+
+
+	
+	public void setVisitTotalCount() throws Exception {
+		Connection con = getConnection();
+		String sql = "INSERT INTO VISIT (V_DATE) VALUES (sysdate)";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.executeUpdate();
+		
+		con.commit();
+	}
+	
+	public int getVisitTotalCount() throws Exception{
+		Connection con = getConnection();
+		String sql = "select count(*) from visit";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		
+		rs.next();
+		int visitor = rs.getInt("count(*)");
+		System.out.println(visitor);
+		con.close();
+		return visitor;
+	}
+	
+	public int getVisitTodayCount() throws Exception{
+		Connection con = getConnection();
+		String sql = "select count(*) from visit where substr(to_char(v_date), 1, 9) = to_date(sysdate, 'yy/MM/dd')";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		
+		rs.next();
+		int todayVisitor = rs.getInt("count(*)");
+		con.close();
+		return todayVisitor;
+	}
+
+	
+}
