351a5961ae2e2e458c4016431ccd2e2c2235f12f
SungWoo824 <scody1219@gmail.com>
2019-12-26 18:12:56 +0900

    kakaopay 1차 구현

---
+++ kh2_semiProject/src/semi/servlet/room_info/RoomReservationErrorServlet.java
@@ -0,0 +1,22 @@
+package semi.servlet.room_info;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/hostel/room_reservation_error.do")
+public class RoomReservationErrorServlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	}
+}
